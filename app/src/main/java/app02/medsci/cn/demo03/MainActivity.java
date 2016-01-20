eaba70cc75b56f131f42453171c19acf60987ed0
louhugh <1305525581@qq.com>
2016-01-20 15:50:10 +0800

    Initial commit

---
+++ app/src/main/java/app02/medsci/cn/demo03/MainActivity.java
@@ -0,0 +1,13 @@
+package app02.medsci.cn.demo03;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
